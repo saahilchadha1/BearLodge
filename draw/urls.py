000c63d8636b0577ded04c3a7e31e46eb06b64c6
Saahil <saahilchadha@berkeley.edu>
2021-08-06 23:53:31 -0500

    New routes, views, templates

--- draw/urls.py
+++ draw/urls.py
@@ -5,6 +5,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
-
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('explore', views.explore, name='explore'),
+    path('explore/<int:listing_id>', views.listing_detail, name='listing_detail'),
+    path('saved', views.saved, name='saved'),
+    path('listings', views.listings, name='listings'),
+    path('listings/create', views.listings_create, name='listings_create'),
+    path('listing/<int:listing_id>', views.listing_edit, name='listings_edit'),
+    path('messages', views.messages, name='messages'),
+    path('messages/<int:user_id>', views.messages_chat, name='messages_chat'),
 ]
--- draw/views.py
+++ draw/views.py
@@ -4,7 +4,32 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'draw/index.html')
 
-def room(request, room_name):
-    return render(request, 'draw/room.html', {
-        'room_name': room_name
-    })
+def login(request):
+    return render(request, 'draw/login.html')
+
+def register(request):
+    return render(request, 'draw/register.html')
+
+def explore(request):
+    return render(request, 'draw/explore.html')
+
+def listing_detail(request, listing_id):
+    return render(request, 'draw/listing_detail.html')
+
+def saved(request):
+    return render(request, 'draw/saved.html')
+
+def listings(request):
+    return render(request, 'draw/listings.html')
+
+def listings_create(request):
+    return render(request, 'draw/listings_create.html')
+
+def listing_edit(request, listing_id):
+    return render(request, 'draw/listing_edit.html')
+
+def messages(request):
+    return render(request, 'draw/messages.html')
+
+def messages_chat(request, user_id):
+    return render(request, 'draw/messages_chat.html')
